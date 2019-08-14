50a0ca9ed0d4e806e97a40ed0d3529fb8df93ea9
Unobtainiumrock <unobtainiumrock@gmail.com>
2019-08-14 02:06:08 -0700

    Initial Commit.

---
+++ drop_messages.js
@@ -0,0 +1,17 @@
+const { spawn } = require('child_process');
+const fs = require('fs');
+const exec = require('child_process').exec;
+const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
+
+// const messages_path = exec('sh find_messages_db_path.sh', (error, stdout, stderr) => {
+
+// });
+
+
+// Get the location of the messages db
+
+// Provide that path to the database function
+
+
+console.log(process.cwd());
